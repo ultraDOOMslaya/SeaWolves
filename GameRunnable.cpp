ee007acc8c9c2db856c710bfcbc4b01211547d79
UltraDoomSlaya <ultraDoomSlaya@gmail.com>
2018-08-06 07:09:26 -0700

    Fixed the OgreTrays mouse cursor issue. Ogre::Rays work now.

--- GameRunnable.cpp
+++ GameRunnable.cpp
@@ -2,7 +2,21 @@
 
 
 
-GameRunnable::GameRunnable() : OgreBites::ApplicationContext("SeaWolves")
+GameRunnable::GameRunnable() 
+	: OgreBites::ApplicationContext("SeaWolves"),
+	mTrayMgr(0),
+	mScnMgr(0),
+	mCam(0),
+	mWindow(0),
+	mRayScnQuery(0),
+	robotAnimState(NULL),
+	mUnitNode(0),
+	mWalkList(0),
+	mWalkSpeed(0),
+	mDirection(0, 0, 0), //direction the object is moving
+	mDistance(0), // distance object has left to travel
+	mDestination(0, 0, 0), // destination the object is moving towards
+	robotEntity(NULL)
 {
 }
 
@@ -12,25 +26,11 @@ GameRunnable::~GameRunnable()
 }
 
 
-bool GameRunnable::keyPressed(const OgreBites::KeyboardEvent& evt)
-{
-	if (evt.keysym.sym == OgreBites::SDLK_ESCAPE)
-	{
-		getRoot()->queueEndRendering();
-	}
-	return true;
-}
-
-bool GameRunnable::mouseMoved(const OgreBites::MouseMotionEvent &evt)
-{
-	return true;
-}
-
-void createSquare(Ogre::SceneManager* mSceneMgr, int width, int height, int edgeLength, std::string meshName, bool oddOrEven, Ogre::ColourValue color)
+void GameRunnable::createSquare(int width, int height, int edgeLength, std::string meshName, bool oddOrEven, Ogre::ColourValue color)
 {
 	Ogre::ManualObject* manual = NULL;
 	{
-		manual = mSceneMgr->createManualObject(meshName);
+		manual = mScnMgr->createManualObject(meshName);
 		bool doIwantToUpdateItLater = false;
 		manual->setDynamic(doIwantToUpdateItLater);
 		float triangleScale = 0.7f;
@@ -84,8 +84,8 @@ void createSquare(Ogre::SceneManager* mSceneMgr, int width, int height, int edge
 		Ogre::String resourceGroup = Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME;
 		manual->convertToMesh(meshName);
 	}
-	Ogre::Entity* planeEntity = mSceneMgr->createEntity(meshName);
-	mSceneMgr->getRootSceneNode()->createChildSceneNode()->attachObject(planeEntity);
+	Ogre::Entity* planeEntity = mScnMgr->createEntity(meshName);
+	mScnMgr->getRootSceneNode()->createChildSceneNode()->attachObject(planeEntity);
 	planeEntity->setCastShadows(false);
 	if (oddOrEven) {
 		planeEntity->setMaterialName("Examples/Rockwall");
@@ -95,7 +95,8 @@ void createSquare(Ogre::SceneManager* mSceneMgr, int width, int height, int edge
 	}
 }
 
-void createTileMap(Ogre::SceneManager* scnMgr)
+
+void GameRunnable::createTileMap(void)
 {
 	int gridWidth = 11;
 	int gridHeight = 11;
@@ -110,68 +111,174 @@ void createTileMap(Ogre::SceneManager* scnMgr)
 		else {
 			xscale += edgeLength;
 		}
-		//OutputDebugStringW(L"Into the Maw");
+		
 		std::stringstream meshName;
 		meshName << i;
 		std::string strToPass = meshName.str();
 
 		if (temp % 2 == 0) {
-			createSquare(scnMgr, xscale, yscale, edgeLength, strToPass, true, (Ogre::ColourValue::Green));
+			createSquare(xscale, yscale, edgeLength, strToPass, true, (Ogre::ColourValue::Green));
 		}
 		else {
-			createSquare(scnMgr, xscale, yscale, edgeLength, strToPass, false, (Ogre::ColourValue::Red));
+			createSquare(xscale, yscale, edgeLength, strToPass, false, (Ogre::ColourValue::Red));
 		}
 	}
 }
+
+
+void GameRunnable::gridSquareCordFinder(std::string squareName)
+{
+	int squareNumber = stoi(squareName);
+	int edgeLength = 50;
+	int dimension = 11;
+	float fedge = edgeLength * 0.7f;
+	int x = 0;
+	int z = 0;
+	int xcount = 0;
+	int ycount = 0;
+	int total = 0;
+	bool end = false;
+	for (xcount; xcount < dimension; xcount++) {
+		total = xcount;
+		while (total <= squareNumber) {
+			if (total == squareNumber) {
+				end = true;
+				break;
+			}
+			else {
+				total += dimension;
+				ycount++;
+			}
+		}
+		if (end) {
+			break;
+		}
+		else {
+			ycount = 0;
+		}
+	}
+
+	x = (xcount * fedge) + (fedge * 0.5);
+	z = (ycount * fedge) + (fedge * 1.5);
+	mWalkList.push_back(Ogre::Vector3(x, 0, z));
+
+	std::ostringstream oss;
+	oss << "Object is: " << squareNumber << " X: " << xcount << " Y: " << ycount;
+	//mCordPanel->setText(oss.str());
+}
+
+
+bool GameRunnable::keyPressed(const OgreBites::KeyboardEvent& evt)
+{
+	
+
+	if (evt.keysym.sym == OgreBites::SDLK_ESCAPE)
+	{
+		getRoot()->queueEndRendering();
+	}
+	
+	return true;
+}
+
+
+bool GameRunnable::mouseMoved(const OgreBites::MouseMotionEvent &evt)
+{
+	//mTrayMgr->refreshCursor();
+	return true;
+}
+
+
+bool GameRunnable::mousePressed(const OgreBites::MouseButtonEvent &evt)
+{
+	bool mbRight = false;
+	
+	if (evt.button == SDL_BUTTON_RIGHT) {
+		mbRight = true;
+	}
+	
+	Ogre::String objectName = "";
+	Ogre::Ray mouseRay = mTrayMgr->getCursorRay(mCam);
+
+	mRayScnQuery->setRay(mouseRay);
+	Ogre::RaySceneQueryResult& result = mRayScnQuery->execute();
+	Ogre::RaySceneQueryResult::iterator it = result.begin();
+
+	for (; it != result.end(); it++)
+	{
+		objectName = it->movable->getName();
+	}
+
+	if (mbRight) {
+		gridSquareCordFinder(objectName);
+		//mWalkList.push_back(Ogre::Vector3(x, 0, y));
+	}
+
+	return true;
+}
+
 
 void GameRunnable::setup(void)
 {	
 	// do not forget to call the base first
 	OgreBites::ApplicationContext::setup();
-
 	addInputListener(this);
 
 	Ogre::Root* root = getRoot();
 	Ogre::ConfigDialog* dialog = OgreBites::getNativeConfigDialog();
+	OgreBites::TrayListener* mTrayListener = new OgreBites::TrayListener();
 
-	scnMgr = root->createSceneManager();
+	mScnMgr = root->createSceneManager();
+	mScnMgr->addRenderQueueListener(getOverlaySystem());
+	mTrayMgr = new OgreBites::TrayManager("InterfaceName", getRenderWindow(), mTrayListener);
+	mTrayMgr->showFrameStats(OgreBites::TrayLocation::TL_BOTTOMLEFT);
+
+	addInputListener(mTrayMgr);
+
 	Ogre::RTShader::ShaderGenerator* shadergen = Ogre::RTShader::ShaderGenerator::getSingletonPtr();
-	shadergen->addSceneManager(scnMgr);
-	Ogre::SceneNode* camNode = scnMgr->getRootSceneNode()->createChildSceneNode("camNode");
-	Ogre::Camera* cam = scnMgr->createCamera("myCam");
+	shadergen->addSceneManager(mScnMgr);
+	camNode = mScnMgr->getRootSceneNode()->createChildSceneNode("camNode");
+	mCam = mScnMgr->createCamera("myCam");
 	
-	camNode->setPosition(400, 600, 800);
+	camNode->setPosition(500, 750, 1000);
 	camNode->lookAt(Ogre::Vector3(0, 0, 0), Ogre::Node::TransformSpace::TS_WORLD);
-	cam->setNearClipDistance(5);
-	camNode->attachObject(cam);
+	mCam->setNearClipDistance(5);
+	camNode->attachObject(mCam);
 
-	Ogre::Viewport* vp = getRenderWindow()->addViewport(cam);
+	Ogre::Viewport* vp = getRenderWindow()->addViewport(mCam);
 	vp->setBackgroundColour(Ogre::ColourValue(0, 0, 0));
-	cam->setAspectRatio(Ogre::Real(vp->getActualWidth()) / Ogre::Real(vp->getActualHeight()));
+	mCam->setAspectRatio(Ogre::Real(vp->getActualWidth()) / Ogre::Real(vp->getActualHeight()));
+	root->addFrameListener(this);
 
-	Ogre::Entity* robotEntity = scnMgr->createEntity("robot.mesh");
+	robotEntity = mScnMgr->createEntity("robot.mesh");
 	robotEntity->setCastShadows(true);
-	scnMgr->getRootSceneNode()->createChildSceneNode()->attachObject(robotEntity);
+	mUnitNode = mScnMgr->getRootSceneNode()->createChildSceneNode("RobotNode", Ogre::Vector3(15.0f, 0.0f, 50.0f));
+	mUnitNode->attachObject(robotEntity);
+	robotAnimState = robotEntity->getAnimationState("Idle");
+	robotAnimState->setLoop(true);
+	robotAnimState->setEnabled(true);
 
-	createTileMap(scnMgr);
+	createTileMap();
 
-	scnMgr->setAmbientLight(Ogre::ColourValue(1.0f, 1.0f, 1.0f));
+	mScnMgr->setAmbientLight(Ogre::ColourValue(1.0f, 1.0f, 1.0f));
 	//scnMgr->setShadowTechnique(Ogre::ShadowTechnique::SHADOWTYPE_STENCIL_MODULATIVE);
 
-	Ogre::Light* spotLight = scnMgr->createLight("SpotLight");
+	Ogre::Light* spotLight = mScnMgr->createLight("SpotLight");
 	spotLight->setDiffuseColour(0, 0, 1.0);
 	spotLight->setSpecularColour(0, 0, 1.0);
 	spotLight->setType(Ogre::Light::LT_SPOTLIGHT);
-	Ogre::SceneNode* spotLightNode = scnMgr->getRootSceneNode()->createChildSceneNode();
+	Ogre::SceneNode* spotLightNode = mScnMgr->getRootSceneNode()->createChildSceneNode();
 	spotLightNode->attachObject(spotLight);
 	spotLightNode->setDirection(-1, -1, 0);
 	spotLightNode->setPosition(Ogre::Vector3(200, 200, 0));
-	spotLight->setSpotlightRange(Ogre::Degree(35), Ogre::Degree(50));
+	spotLight->setSpotlightRange(Ogre::Degree(35), Ogre::Degree(50));	
 	
+	mRayScnQuery = mScnMgr->createRayQuery(Ogre::Ray());
+	mRayScnQuery->setSortByDistance(true);
 }
 //----------------------------------------------------------------
 void GameRunnable::frameRendered(const Ogre::FrameEvent& evt)
 {
+	/* Camera movement */
 	Ogre::Vector3 transVector = Ogre::Vector3::ZERO;
 	int mMoveScale = 250;
 	
@@ -190,10 +297,64 @@ void GameRunnable::frameRendered(const Ogre::FrameEvent& evt)
 	else if (y < 20) {
 		transVector.y += mMoveScale;
 	}
+
+	/* Robot animation and movement */
+	if (robotAnimState != NULL) {
+		if (mDirection == Ogre::Vector3::ZERO) {
+
+			if (nextLocation()) {
+				Ogre::Vector3 src = mUnitNode->getOrientation() * Ogre::Vector3::UNIT_X;
+
+				if ((1.0 + src.dotProduct(mDirection)) < 0.0001)
+				{
+					mUnitNode->yaw(Ogre::Degree(180));
+				}
+				else
+				{
+					Ogre::Quaternion quat = src.getRotationTo(mDirection);
+					mUnitNode->rotate(quat);
+				}
+				robotAnimState = robotEntity->getAnimationState("Walk");
+				robotAnimState->setLoop(true);
+				robotAnimState->setEnabled(true);
+			}
+
+		}
+		else {
+			Ogre::Real move = mWalkSpeed * evt.timeSinceLastFrame;
+			mDistance -= move;
+			if (mDistance <= 0.0f) {
+				mUnitNode->setPosition(mDestination);
+				mDirection = Ogre::Vector3::ZERO;
+
+				// Consider adding rotation code (intermediate tutorial1)
+				robotAnimState = robotEntity->getAnimationState("Idle");
+				robotAnimState->setLoop(true);
+				robotAnimState->setEnabled(true);
+
+			}
+			else {
+				mUnitNode->translate(move * mDirection);
+			}
+		}
+
+
+		robotAnimState->addTime(evt.timeSinceLastFrame);
+	}
 	
-	scnMgr->getSceneNode("camNode")->translate(transVector * evt.timeSinceLastFrame, Ogre::Node::TS_LOCAL);
+	mScnMgr->getSceneNode("camNode")->translate(transVector * evt.timeSinceLastFrame, Ogre::Node::TS_LOCAL);
 }
 
+
+bool GameRunnable::nextLocation()
+{
+	if (mWalkList.empty()) { return false; }
+	mDestination = mWalkList.front();  // this gets the front of the deque
+	mWalkList.pop_front();             // this removes the front of the deque
+	mDirection = mDestination - mUnitNode->getPosition();
+	mDistance = mDirection.normalise();
+	return true;
+}
 
 
 #if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
--- GameRunnable.h
+++ GameRunnable.h
@@ -5,6 +5,8 @@
 #include <Ogre.h>
 #include <OgreApplicationContext.h>
 #include <OgreBitesConfigDialog.h>
+#include <OgreOverlay.h>
+#include <OgreTrays.h>
 #include <SDL.h>
 
 
@@ -18,11 +20,30 @@ public:
 	void setup(void);
 	virtual bool keyPressed(const OgreBites::KeyboardEvent& evt);
 	virtual bool mouseMoved(const OgreBites::MouseMotionEvent &evt);
-	void frameRendered(const Ogre::FrameEvent& evt);
+	virtual bool mousePressed(const OgreBites::MouseButtonEvent &evt);
+	virtual bool nextLocation();
+	
+	virtual void frameRendered(const Ogre::FrameEvent& evt);
+	virtual void gridSquareCordFinder(std::string squareName);
 
-	Ogre::SceneManager*			scnMgr;
-	Ogre::Camera*				cam;
+	virtual void createSquare(int width, int height, int edgeLength, std::string meshName, bool oddOrEven, Ogre::ColourValue color);
+	virtual void createTileMap(void);
+
+	OgreBites::TrayManager*		mTrayMgr;
+
+	Ogre::SceneManager*			mScnMgr;
+	Ogre::Camera*				mCam;
 	Ogre::RenderWindow*         mWindow;
+	Ogre::RaySceneQuery*		mRayScnQuery;
+	Ogre::AnimationState*		robotAnimState;
+	Ogre::SceneNode*			mUnitNode;
+	std::deque<Ogre::Vector3>   mWalkList;
+	Ogre::Real					mWalkSpeed;
+	Ogre::Vector3				mDirection; //direction the object is moving
+	Ogre::Real					mDistance; // distance object has left to travel
+	Ogre::Vector3				mDestination; // destination the object is moving towards
+	Ogre::Entity*				robotEntity;
+	Ogre::SceneNode*			camNode;
 };
 
 #endif __GameRunnable_h_
